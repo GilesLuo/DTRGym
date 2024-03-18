e837f9324ec7cdccee59a38125f16418c4e34c64
Zhiyao Luo <luozhiyao933@126.com>
2024-03-18 14:57:25 +0000

    try workflow

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_version() -> str:
 
 def get_install_requires():
     return [
-        "tianshou",
+        "tianshou==0.5.0",
         "torch>=1.13.0",
         "packaging",
         "tqdm",
